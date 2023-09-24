7d4562b81c6001c1868ad1c7bb3cd195b7b2ddad
Leonardo Sainz <leo.database92@gmail.com>
2023-09-24 00:00:16 -0300

    feat: cambo tipado characterState

--- app/store/index.tsx
+++ app/store/index.tsx
@@ -1,19 +1,23 @@
 import create from "zustand";
 
 interface CharacterState {
-  characterOneEpisodes: [];
-  setCharacterOneEpisodes: (by: []) => void;
-  characterTwoEpisodes: [];
-  setCharacterTwoEpisodes: (by: []) => void;
+  characterOne: { episode: string[]; name: string };
+  setCharacterOne: (by: { episode: string[]; name: string }) => void;
+  characterTwo: { episode: string[]; name: string };
+  setCharacterTwo: (by: { episode: string[]; name: string }) => void;
 }
-const useStore = create<CharacterState>()((set) => ({
-  characterOneEpisodes: [],
-  characterTwoEpisodes: [],
 
-  setCharacterOneEpisodes: (characterOneEpisodes: []) =>
-    set({ characterOneEpisodes }),
-    setCharacterTwoEpisodes: (characterTwoEpisodes: []) =>
-    set({ characterTwoEpisodes }),
+const useStore = create<CharacterState>()((set) => ({
+  characterOne: {
+    episode: [],
+    name: "",
+  },
+  characterTwo: {
+    episode: [],
+    name: "",
+  },
+  setCharacterOne: (characterOne) => set({ characterOne }), // Tipo actualizado
+  setCharacterTwo: (characterTwo) => set({ characterTwo }), // Tipo actualizado
 }));
 
 export default useStore;
