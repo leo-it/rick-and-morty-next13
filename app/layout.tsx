041bbbb0faa221c859b103b439d8a17d3dc717e9
Leonardo Sainz <leo.database92@gmail.com>
2023-09-25 21:27:05 -0300

    feat: limpieza de cosas que no se usan

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,29 @@
-import './globals.css'
+import "./globals.css";
 
-import { Footer } from './components/Footer'
-import { Inter } from 'next/font/google'
-import type { Metadata } from 'next'
-import { Navbar } from './components'
+import { Footer } from "./components/Footer";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import { Navbar } from "./components";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Rick and Morty App',
-  description: 'Characters and episodes of the rick and morty series ',
-}
+  title: "Rick and Morty App",
+  description: "Characters and episodes of the rick and morty series ",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
-        {children}  <Footer/></body>
-     
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
