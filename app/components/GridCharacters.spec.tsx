82cf970deddc5d8d912778c25bde8c413d71c9bf
Leonardo Sainz <leo.database92@gmail.com>
2023-09-28 19:50:20 -0300

    feat: agrego test para cuano no  trae data fetch

--- app/components/GridCharacters.spec.tsx
+++ app/components/GridCharacters.spec.tsx
@@ -9,21 +9,33 @@ const characters = [
     status: "Alive",
     species: "Human",
     image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-    episode: [
-      "https://rickandmortyapi.com/api/episode/25",
-    ],
+    episode: ["https://rickandmortyapi.com/api/episode/25"],
   },
 ];
 describe("Characters Grid component", () => {
   it("render correctly", () => {
     render(<GridCharacters characters={characters} />);
 
+    const characterOneHeader = screen.getByText("Character #1");
+    const characterTwoHeader = screen.getByText("Character #2");
     const nameElement = screen.getByText("Rick Sanchez");
     const status = screen.getByText("Alive");
     const specie = screen.getByText("Human");
 
+    expect(characterOneHeader).toBeTruthy();
+    expect(characterTwoHeader).toBeTruthy();
+    expect(characterOneHeader).not.toEqual(characterTwoHeader);
+    expect(characterOneHeader).toBeTruthy();
     expect(nameElement).toBeTruthy();
     expect(status).toBeTruthy();
     expect(specie).toBeTruthy();
+  });
+
+  it("displays 'Characters dont found' message when no characters are provided", () => {
+    render(<GridCharacters characters={[]} />);
+  
+    const message = screen.getByText("Characters dont found");
+  
+    expect(message).toBeTruthy();
   });
 });
