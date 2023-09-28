2cc13608bc20cd703119c2d0459c977c01e7eb26
Leonardo Sainz <leo.database92@gmail.com>
2023-09-28 19:23:04 -0300

    feat: agrego test boton select

--- app/components/Card.spec.tsx
+++ app/components/Card.spec.tsx
@@ -1,14 +1,17 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 import { Card } from "./Card";
 
 let isSelected = 0;
+let setIsSelected = (id) => {
+  isSelected = id;
+};
 describe("Character card component", () => {
   it("render correctly", () => {
     render(
       <Card
-        isSelected={0}
-        setIsSelected={isSelected}
+        isSelected={isSelected}
+        setIsSelected={setIsSelected}
         name={"Rick Sanchez"}
         status={"Alive"}
         specie={"Human"}
@@ -24,5 +27,26 @@ describe("Character card component", () => {
     expect(nameElement).toBeTruthy();
     expect(status).toBeTruthy();
     expect(specie).toBeTruthy();
+  });
+  it("selects the card when 'Select' button is clicked", () => {
+    render(
+      <Card
+        isSelected={isSelected}
+        setIsSelected={setIsSelected}
+        name={"Rick Sanchez"}
+        status={"Vivo"}
+        specie={"Humano"}
+        image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg?w=256"}
+        id={1}
+      />
+    );
+
+    const selectButton = screen.getByText("Select");
+
+    expect(isSelected).toBe(0);
+
+    fireEvent.click(selectButton);
+
+    expect(isSelected).toBe(1);
   });
 });
