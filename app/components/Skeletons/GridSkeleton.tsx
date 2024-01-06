d2e450dd3996bba4ac439a2c88b7ae0816551a6a
Leonardo Sainz <leo.database92@gmail.com>
2024-01-06 18:34:04 -0300

    fix: ts props

--- app/components/Skeletons/GridSkeleton.tsx
+++ app/components/Skeletons/GridSkeleton.tsx
@@ -1,7 +1,7 @@
 import ContentLoader from "react-content-loader";
 import React from "react";
 
-const GridSkeleton = (props) => (
+const GridSkeleton = (props: any) => (
   <ContentLoader
     style={{ width: "100%" }}
     speed={2}
@@ -9,11 +9,16 @@ const GridSkeleton = (props) => (
     viewBox="0 0 1100 444"
     {...props}
   >
-    
-    <rect x="50" y="23" rx="2" ry="2" style={{ width: "45%", height:"70%" }}  />
-    <rect x="564" y="23" rx="0" ry="0" style={{ width: "45%", height:"70%" }} />
-    <rect x="487" y="350" rx="27" ry="27" width="38" height="30" /> 
-    <rect x="582" y="350" rx="27" ry="27" width="38" height="30" /> 
+    <rect x="50" y="23" rx="2" ry="2" style={{ width: "45%", height: "70%" }} />
+    <rect
+      x="564"
+      y="23"
+      rx="0"
+      ry="0"
+      style={{ width: "45%", height: "70%" }}
+    />
+    <rect x="487" y="350" rx="27" ry="27" width="38" height="30" />
+    <rect x="582" y="350" rx="27" ry="27" width="38" height="30" />
     <rect x="537" y="350" rx="0" ry="0" width="38" height="30" />
   </ContentLoader>
 );
